6a38834cea5a565580fcf2c740beb240052f6bae
gojgho <60379311+gojgho-coder@users.noreply.github.com>
2024-02-27 22:16:38 +0900

    logger add

--- src/main/kotlin/com/myhome/library/service/member/MemberService.kt
+++ src/main/kotlin/com/myhome/library/service/member/MemberService.kt
@@ -3,6 +3,7 @@ package com.myhome.library.service.member
 import com.myhome.library.domain.member.Member
 import com.myhome.library.dto.member.MemberDto
 import com.myhome.library.repository.member.MemberRepository
+import com.myhome.library.utils.logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -11,6 +12,8 @@ import org.springframework.transaction.annotation.Transactional
 class MemberService @Autowired constructor(
     private val memberRepository: MemberRepository,
 ){
+
+    private val log = logger()
 
     @Transactional
     fun saveUser(memberDto: MemberDto) {
--- src/main/kotlin/com/myhome/library/utils/LoggerSupportUtils.kt
+++ src/main/kotlin/com/myhome/library/utils/LoggerSupportUtils.kt
@@ -3,7 +3,5 @@ package com.myhome.library.utils
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-class LoggerSupportUtils {
+    inline fun <reified T : Any> T.logger(): Logger = LoggerFactory.getLogger(T::class.java)
 
-    inline fun <reified T : Any> Logger.logger(): Logger = LoggerFactory.getLogger(T::class.java)
-}
