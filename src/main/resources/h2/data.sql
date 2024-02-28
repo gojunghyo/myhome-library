e5a20c56856aa50463491d7bef25d35b3a0479fa
ITO_MZ_GOJGHO\mz_gojgho <gojgho@mz.co.kr>
2024-02-28 16:28:55 +0900

    [data] 기본 스키마 데이터 수동 등록

---
+++ src/main/resources/h2/data.sql
@@ -0,0 +1,67 @@
+-- [BOOK] ADD    TYPE = [COMPUTER ECONOMY SOCIETY LANGUAGE SCIENCE SELF COMIC NORMAL]
+
+insert into book (book_seq, isbn, name, type)
+values  (default, 9791168473690, '세이노의 가르침', 'SELF');
+
+
+insert into book (book_seq, isbn, name, type)
+values  (default, 9791136783134,  '주술회전 25: 인외마경 신주쿠 결전', 'COMIC');
+
+
+insert into book (book_seq, isbn, name, type)
+values  (default, 9788901276533, '나는 메트로폴리탄 미술관의 경비원입니다', 'NORMAL');
+
+
+insert into book (book_seq, isbn, name, type)
+values  (default, 9791192300818, '마흔에 읽는 쇼펜하우어', 'SELF');
+
+
+insert into book (book_seq, isbn, name, type)
+values  (default, 9791191777628, '내 삶을 지키는 바운더리', 'SELF');
+
+
+insert into book (book_seq, isbn, name, type)
+values  (default, 9788997743605, '퍼스널 MBA(10주년 기념 증보판)', 'SELF');
+
+
+insert into book (book_seq, isbn, name, type)
+values  (default, 9791187799252,  '공장 투자 이렇게 쉬웠어?', 'SELF');
+
+insert into book (book_seq, isbn, name, type)
+values  (default, 9791193506202, '아이는 무엇으로 자라는가', 'SELF');
+
+insert into book (book_seq, isbn, name, type)
+values  (default, 9791190227339, '항복론: 성공을 위한 내려놓기', 'SELF');
+
+insert into book (book_seq, isbn, name, type)
+values  (default, 9788965422785, '해커스 토익 기출 VOCA(보카)', 'SELF');
+
+insert into book (book_seq, isbn, name, type)
+values  (default, 9791171251179, '나는 행복한 푸바오 할부지입니다', 'SELF');
+
+insert into book (book_seq, isbn, name, type)
+values  (default, 9788965424772, '해커스 토익 LC Listening(리스닝) 기본서', 'SELF');
+
+insert into book (book_seq, isbn, name, type)
+values  (default, 9788998441012, '모순', 'SELF');
+
+insert into book (book_seq, isbn, name, type)
+values  (default, 9791198621504, '발상의 회로', 'SELF');
+
+insert into book (book_seq, isbn, name, type)
+values  (default, 9791130646381, '이처럼 사소한 것들', 'SELF');
+
+insert into book (book_seq, isbn, name, type)
+values  (default, 9788917239508, 'ETS 토익 정기시험 기출문제집', 'SELF');
+
+insert into book (book_seq, isbn, name, type)
+values  (default, 9791193506332, '무례한 친구가 생겼어요', 'SELF');
+
+insert into book (book_seq, isbn, name, type)
+values  (default, 9788901280516, '마음 해방', 'SELF');
+
+insert into book (book_seq, isbn, name, type)
+values  (default, 9791160274455, '여행 드롭', 'SELF');
+
+insert into book (book_seq, isbn, name, type)
+values  (default, 9791193080146, '느리게 나이 드는 습관', 'SELF');
